2f9bf20d35c1a665e0e9b63dde409599c8c4db7a
Rohan Mishra <rohanmishra.004@gmail.com>
2023-07-08 15:09:55 +0530

    Arrays

---
+++ lesson4/Demo1.java
@@ -0,0 +1,11 @@
+//Arrays
+public class Demo1 {
+    public static void main(String a[]) {
+        // int nums[] = new int[5]; // creating an array of size 5
+        int nums[] = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
